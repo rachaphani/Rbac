17bc61024102745ffc1dfc48416e07f6d0d28549
rachaphani <66510167+rachaphani@users.noreply.github.com>
2024-11-28 19:10:26 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,16 @@
+const firebaseConfig = {
+apiKey: "AIzaSyCxPED6wZjdfH9I5R7FnUsdStaggcBg8FU",
+authDomain: "rbacphani.firebaseapp.com",
+databaseURL: "https://rbacphani-default-rtdb.firebaseio.com/",
+projectId: "rbacphani",
+storageBucket: "rbacphani.firebasestorage.app",
+messagingSenderId: "1055101653505",
+appId: "1:1055101653505:web:d8cbe2e5f63f2a5ca46600",
+measurementId: "G-6D7TW2VZPV"
+
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
+
+
